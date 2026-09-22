d7b2c6bc9374a18ba37a913586dff32167a60853
orgSolutions <colaboradorwandersonamarante@gmail.com>
2026-09-22 20:51:09 -0300

    update

--- android/app/src/main/java/com/fullsolucions/solutarquive/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.fullsolucions.solutarquive;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.fullsolucions.solutarquive',
-  appName: 'Solut_Arquive',
-  webDir: 'www'
-};
-
-export default config;
